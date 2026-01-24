7e4102f2602347e8178ba8e4e7facff2e24d7330
joa23 <stefangroschupf@gmail.com>
2026-01-23 16:42:12 -0800

    chore: Update Homebrew formula to v1.2.3

--- Formula/linear-cli.rb
+++ Formula/linear-cli.rb
@@ -4,28 +4,28 @@
 class LinearCli < Formula
   desc "Token-efficient CLI for Linear"
   homepage "https://github.com/joa23/linear-cli"
-  version "1.2.2"
+  version "1.2.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/joa23/linear-cli/releases/download/v#{version}/linear-cli_Darwin_arm64.tar.gz"
-      sha256 "538f2555e4d8ed9e995c47ec3e1f4bd8f7254a636b9da23a81819772662bf612"
+      sha256 "c6d3586e5e6b2d07c26af4443f98be5446b692da662d58a2e8e27dde2153dc83"
     end
     on_intel do
       url "https://github.com/joa23/linear-cli/releases/download/v#{version}/linear-cli_Darwin_x86_64.tar.gz"
-      sha256 "0afc5a530de9984502f7963375f6d0b0d15f8d5ebca9568fcf0e2a59e08584cd"
+      sha256 "69a2dafd88800d87242794b1854cb54ea0ca9d776609de44a7aa97a48a91c0e3"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/joa23/linear-cli/releases/download/v#{version}/linear-cli_Linux_arm64.tar.gz"
-      sha256 "349834eccb8a6b9c960254a9661fd83c6d4579445494ee980281be936b5ec922"
+      sha256 "efbcd531bc980a9feafc1c33fdcd784d4bcb4f27596ada3518c6d23e4cd835b6"
     end
     on_intel do
       url "https://github.com/joa23/linear-cli/releases/download/v#{version}/linear-cli_Linux_x86_64.tar.gz"
-      sha256 "8ce5899dd06bbd7846b55412ec5d92463b280638624d617acc9741fbb06c43a3"
+      sha256 "f83b466d545ef4b0ec98cf1f69e798a2757a1b83253bdd372f103e998e06bbf8"
     end
   end
 
